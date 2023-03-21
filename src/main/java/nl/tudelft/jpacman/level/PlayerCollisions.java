84d2945783efb5f21b8956c5fd0c6d4ea09dc8a7
Thibault de Combejean <minoterrius@yahoo.fr>
2023-03-20 20:59:05 -0400

    Refactor playerCollision.java
    Suppression de méthodes redondantes et regroupement du code pour plus de clarté

--- src/main/java/nl/tudelft/jpacman/level/PlayerCollisions.java
+++ src/main/java/nl/tudelft/jpacman/level/PlayerCollisions.java
@@ -33,37 +33,14 @@ public class PlayerCollisions implements CollisionMap {
     @Override
     public void collide(Unit mover, Unit collidedOn) {
         if (mover instanceof Player) {
-            playerColliding((Player) mover, collidedOn);
-        }
-        else if (mover instanceof Ghost) {
-            ghostColliding((Ghost) mover, collidedOn);
-        }
-        else if (mover instanceof Pellet) {
-            pelletColliding((Pellet) mover, collidedOn);
-        }
-    }
-
-    private void playerColliding(Player player, Unit collidedOn) {
-        if (collidedOn instanceof Ghost) {
-            playerVersusGhost(player, (Ghost) collidedOn);
-        }
-        if (collidedOn instanceof Pellet) {
-            playerVersusPellet(player, (Pellet) collidedOn);
+            if (collidedOn instanceof Ghost) {
+                playerVersusGhost((Player) mover, (Ghost) collidedOn);
+            }
+            if (collidedOn instanceof Pellet) {
+                playerVersusPellet((Player) mover, (Pellet) collidedOn);
+            }
         }
     }
-
-    private void ghostColliding(Ghost ghost, Unit collidedOn) {
-        if (collidedOn instanceof Player) {
-            playerVersusGhost((Player) collidedOn, ghost);
-        }
-    }
-
-    private void pelletColliding(Pellet pellet, Unit collidedOn) {
-        if (collidedOn instanceof Player) {
-            playerVersusPellet((Player) collidedOn, pellet);
-        }
-    }
-
 
     /**
      * Actual case of player bumping into ghost or vice versa.
